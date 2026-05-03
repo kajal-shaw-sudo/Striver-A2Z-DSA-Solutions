00a332f19810bbe0e58cb7382bbdce3b3d890891
Kajal <kajalshaw28b@gmail.com>
2026-05-03 13:04:51 +0530

    patterns2

---
+++ .history/Basics/Pattern-20_20260503124009.cpp
@@ -0,0 +1,16 @@
+/*
+problem: Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+Print the pattern in the function given to you.
+*/
+
---
+++ .history/Basics/Pattern-7_20260503125233.cpp
@@ -0,0 +1,38 @@
+/*
+problem: Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+    *
+   ***
+  *****
+ *******
+*********
+
+
+Print the pattern in the function given to you.
+*/
+
+class Solution {
+public:
+    void pattern7(int n) {
+        for (int i=0; i<n; i++) {
+            // (n-i-1) leading spaces
+            for (int j=0; j<n-i-1; j++) {
+                cout<<' ';
+            }
+
+            // (2*i+1) stars
+            for (int j=0; j<2*i+1; j++) {
+                cout<<'*';
+            }
+
+            // (n-i-1) trailing spaces (optional)
+            /*for (int j=0; j<n-i-1; j++) {
+                cout<<' ';
+            }
+            */
+
+            cout<<'\n';
+        }
+    }
+};
+// tc: O(n^2), sc: O(1)
